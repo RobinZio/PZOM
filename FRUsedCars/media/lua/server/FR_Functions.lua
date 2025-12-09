b9a5304502ef06b6fd03ee835251bc17187b7d90
RobinZio <67667370+RobinZio@users.noreply.github.com>
2025-12-09 20:21:49 +0900

    Delete FRUsedCars directory

--- FRUsedCars/media/lua/server/FR_Functions.lua
+++
@@ -1,7 +0,0 @@
-require "Vehicle/Vehicles"
-
-function Vehicles.ContainerAccess.FR_VehicleArmory(vehicle, part, chr)
-    if chr:getVehicle() == vehicle then
-        return true
-    end
-end
--- FRUsedCars/media/lua/shared/FR_Icons.lua
+++
@@ -1,4 +0,0 @@
-require "Definitions/ContainerButtonIcons"
-
-ContainerButtonIcons.FR_VehicleArmory = getTexture("media/textures/FR_VehicleArmory.png")
-ContainerButtonIcons.FR_VehicleArmory2 = getTexture("media/textures/FR_VehicleArmory.png")
